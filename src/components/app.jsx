1d466fc4fe339eb57ed792f3136fe096fee22556
aurelie mathys <mathyau@gmail.com>
2020-01-06 14:54:59 -0500

    searchbar effect

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,8 +2,23 @@ import React, { Component } from 'react';
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
+import giphy from 'giphy-api';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [],
+      selectedGifId: "eA2qEgL5mAsOk"
+    }
+  }
+
+search = (query) => {
+
+// API call
+
+}
+
   render() {
     const gifs = [
       { id: "eA2qEgL5mAsOk"},
@@ -14,11 +29,11 @@ class App extends Component {
         <div className="left-scene">
           <SearchBar />
             <div className="selected-gif">
-              <Gif id="eA2qEgL5mAsOk"/>
+              <Gif id={this.state.selectedGifId}/>
             </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs} />
+          <GifList gifs={this.state.gifs} />
         </div>
       </div>
     );
--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -5,7 +5,7 @@ import Gif from './gif.jsx';
 class GifList extends Component {
   render() {
     return this.props.gifs.map(gif => {
-      return <Gif id={gif.id} />
+      return <Gif id={gif.id} key={gif.id} />
     });
 
   }
