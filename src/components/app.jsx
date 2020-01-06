5a47001e2fb0be4b4f50789e7cd9cc51bcd5238f
aurelie mathys <mathyau@gmail.com>
2020-01-05 21:18:38 -0500

    ajout searchbar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
 
 class App extends Component {
   render() {
-  return(
-    <div>
-      <div className="left-scene"></div>
-      <div className="right-scene"></div>
-    </div>
+    const gifs = [
+      { id: "IFpVKDqOo84F2"},
+      { id: "RMtvA0H3W3a3S"}
+    ];
+    return(
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+            <div className="selected-gif">
+              <Gif />
+            </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={gifs} />
+        </div>
+      </div>
     );
-
   }
 }
 
 export default App;
-
---
+++ src/components/gif_list.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GifList extends Component {
+  render() {
+    this.props.gifs.map(gif => {
+
+    });
+
+  }
+
+}
+
+export default GifList;
